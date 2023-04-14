484c31b4b6366a66bb8fc766d64b232b5750c07d
CarlosIgreda <cjigredarivas@gmail.com>
2023-04-14 14:07:01 -0500

    Solve task 1

---
+++ task1.js
@@ -0,0 +1,5 @@
+const stringLength = (string) => {
+    return string.length;
+}
+
+module.exports = stringLength;
---
+++ task1.test.js
@@ -0,0 +1,15 @@
+const stringLength = require('./task1');
+
+test('the string is at least 1 character long and not longer than 10 characters', () => {
+    const str = "Hi";
+    expect(stringLength(str)).toBeGreaterThanOrEqual(1);
+    expect(stringLength(str)).toBeLessThanOrEqual(10);
+
+});
+
+test('the string is at least 1 character long and not longer than 10 characters', () => {
+    const str = "Hi buddy how are you";
+    expect(stringLength(str)).toBeGreaterThanOrEqual(1);
+    expect(stringLength(str)).toBeLessThanOrEqual(10);
+    
+});
